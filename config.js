8e64b2978bc995deaf10c53efbd0e35ec3a27e15
Gabriel Mendes <103916287+GabrielMendes94@users.noreply.github.com>
2023-10-12 04:30:43 +0000

    db sequelize

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        username: 'root', //'seu_usuario_do_banco_de_dados',
+        password: 'root', //'sua_senha_do_banco_de_dados',
+        database: 'produto', //'nome_do_banco_de_dados',
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+};
